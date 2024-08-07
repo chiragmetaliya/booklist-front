1194e980b8ecfca81ef43403a68ec90e05dec586
chiragmetaliya <chiragmetaliya@gmail.com>
2024-08-07 19:50:48 -0400

    Initial commit

---
+++ src/components/Cards.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Cards({ item }) {
+  return (
+    <>
+      <div className="mt-4 my-3 p-3">
+        <div className="card w-92 bg-base-100 shadow-xl hover:scale-105 duration-200 dark:bg-slate-900 dark:text-white dark:border">
+          <figure>
+            <img src={item.image} alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              {item.name}
+              <div className="badge badge-secondary">{item.category}</div>
+            </h2>
+            <p>{item.title}</p>
+            <div className="card-actions justify-between">
+              <div className="badge badge-outline">${item.price}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Cards;
