b60b9bd2a3366a423a8cacb2775a830f2865b87b
chiragmetaliya <chiragmetaliya@gmail.com>
2024-08-07 20:35:06 -0400

    Fixing redirection

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Home from "./home/Home";
-import { Navigate, Route, Routes } from "react-router-dom";
 import Courses from "./courses/Courses";
 import Signup from "./components/Signup";
 import { Toaster } from "react-hot-toast";
 import { useAuth } from "./context/AuthProvider";
 
 function App() {
-  const [authUser, setAuthUser] = useAuth();
-  console.log(authUser);
+  const [authUser] = useAuth();
+  const [view, setView] = useState("home"); // State to manage which view to show
+
   return (
     <>
       <div className="dark:bg-slate-900 dark:text-white">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/course"
-            element={authUser ? <Courses /> : <Navigate to="/signup" />}
-          />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
+        {authUser ? (
+          <>
+            <Navbar setView={setView} /> {/* Pass setView to Navbar */}
+            {view === "home" && <Home />}
+            {view === "courses" && <Courses />}
+          </>
+        ) : (
+          <Signup />
+        )}
         <Toaster />
       </div>
     </>
--- src/courses/Courses.jsx
+++ src/courses/Courses.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Course from "../components/Course";
 import Footer from "../components/Footer";
+
 function Courses() {
   return (
     <>
